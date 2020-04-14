a2ab479cd8fc3acde7fa6d7789161c5f6f398d75
dgabed <dtgabriel@gmail.com>
2020-04-14 10:09:49 -0300

    Agregada la lista de alumnos correctamente

--- EntidadNegocio/Curso.cs
+++ EntidadNegocio/Curso.cs
@@ -8,6 +8,17 @@ namespace EntidadNegocio
 {
     class Curso
     {
-        
+        private string descripcion;
+        private List <Alumno> alumnos= new List <Alumno>();
+
+        public void agregarAlumno(Alumno a)
+        {
+            this.alumnos.Add(a);
+        }
+
+        public List<Alumno> getAlumnos()
+        {
+            return this.alumnos;
+        }
     }
 }
