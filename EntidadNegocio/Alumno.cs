9962b3408cab6213e8929fb092affe6528912329
dgabed <dtgabriel@gmail.com>
2020-04-14 09:34:47 -0300

    Primer commit con la clase alumno OK

---
+++ EntidadNegocio/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadNegocio
+{
+    class Alumno
+    {
+        public int id { get; set; }
+        string nombre;
+        string apellido;
+        string direccion;
+        string telefono;
+
+
+    }
+}
