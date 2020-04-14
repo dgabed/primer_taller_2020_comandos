6939a7a296b697975078c5bd423aaf457d15b1e9
dgabed <dtgabriel@gmail.com>
2020-04-14 09:58:00 -0300

    Clase curso agregada

--- EntidadNegocio/Curso.cs
+++ EntidadNegocio/Curso.cs
@@ -8,6 +8,17 @@ namespace EntidadNegocio
 {
     class Curso
     {
-        
+        private string descripcion;
+        private Alumno alumno;
+
+        public void setAlumno(Alumno a)
+        {
+            this.alumno = a;
+        }
+
+        public Alumno getAlumno()
+        {
+            return this.alumno;
+        }
     }
 }
